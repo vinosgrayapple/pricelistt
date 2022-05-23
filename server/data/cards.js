d64cbe4ba5827099929f5a56fd9ffa8bd21f18d6
Maxim Stanovskiy (home) <maxim.stanovskiy@gmail.com>
2022-05-23 21:45:18 +0300

    заброс объекта с серва

---
+++ server/data/cards.js
@@ -0,0 +1,37 @@
+
+ const cards1={
+    id:1,
+    titleText:"РАСПРОДАЖА",
+
+    titleImage:"pictures/Title1.png",
+
+    productImage:"pictures/ice_cream1.png",
+
+    label:{},
+
+    labelImage:"pictures/Label1.png",
+
+    labelText:"-11%",
+    
+    descriptionField:{}, 
+
+    productNameText:"Черничное мороженое",
+
+    productDescriptionText: "В вафельном рожке, 80г",
+
+    currentPrice: "18 грн",
+
+    oldPrice:"20 грн",
+
+    cardBottom:{},
+
+    bottomImage:{},
+
+    bottomLogo:"pictures/Logo1.png",
+
+    bottomText:" Данная акция не является настоящей рекламой, а является всего лишь результатом выполнения тестового задания"
+
+
+}
+
+module.exports = {cards1}
